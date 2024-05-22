9339db61dca71d8eb750d453cd78dabcaee06968
alina <alkovryga@gmail.com>
2024-05-22 22:24:27 +0300

    shop working

--- shop/views.py
+++ shop/views.py
@@ -3,14 +3,20 @@ from .models import Category
 from product.models import Product
 
 
-# Create your views here.
 def shop(request):
     categories = Category.objects.all()
     products = Product.objects.filter(is_visible=True)
+    selected_category_slug = request.GET.get('category', None)
+
+    if selected_category_slug:
+        selected_category = Category.objects.filter(slug=selected_category_slug).first()
+        products = products.filter(category=selected_category)
+        categories_choice = [selected_category]
+    else:
+        categories_choice = [categories.first()] if categories else []
+
     return render(request, 'shop_content.html', context={
         'categories': categories,
         'products': products,
-        'categories_choise': [categories[0]],
-
+        'categories_choice': categories_choice,
     })
-# Create your views here.
