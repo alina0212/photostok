6ad28ea1839973c23c56289dc182171080432d1d
Polina <ivanetspolina@knu.ua>
2024-05-26 18:04:41 +0300

    ..

--- photostok/urls.py
+++ photostok/urls.py
@@ -3,7 +3,7 @@ URL configuration for photostok project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
+
 """
 from django.contrib import admin
 from django.urls import path
